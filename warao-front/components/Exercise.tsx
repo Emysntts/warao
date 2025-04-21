4b1e4396dd466f56224364fcafaf10029959f1ef
JonatasMSS <jonatas.miguelss@gmail.com>
2025-04-21 20:39:04 -0300

    [Feature] - Add ExerciseComp component for exercise display

---
+++ warao-front/components/Exercise.tsx
@@ -0,0 +1,40 @@
+import { Ionicons } from '@expo/vector-icons';
+import { useFonts, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+type ExerciseCompProps = {
+  text: string;
+  image?: string;
+  isUnlocked?: boolean;
+  onPress?: () => void;
+};
+
+const ExerciseComp = (props: ExerciseCompProps) => {
+  const [fonts] = useFonts({ Poppins_600SemiBold });
+
+  if (!fonts) return null;
+
+  return (
+    <TouchableOpacity activeOpacity={0.8} onPress={props.onPress} className="my-3">
+      <View className="flex  w-full flex-row  items-center justify-between rounded-lg bg-white p-5">
+        <View className="flex flex-row items-center gap-5">
+          <View className="h-16 w-16 rounded-full bg-blue-950 " />
+          <Text
+            style={{
+              fontFamily: 'Poppins_600SemiBold',
+            }}
+            className="text-warao-green-dark">
+            {props.text}
+          </Text>
+        </View>
+        {props.isUnlocked ? (
+          <Ionicons name="lock-open-outline" size={28} color="#747F64" />
+        ) : (
+          <Ionicons name="lock-closed-outline" size={28} color="#747F64" />
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ExerciseComp;
