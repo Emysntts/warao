ebc46413282568988d703bbb0787e2ac93499b4c
Vitoreism <vitoreism@gmail.com>
2025-04-28 13:23:48 -0300

    Dados dos glossários (Ainda incompletos)

---
+++ data/glossario_1/dicionarios_separados.py
@@ -0,0 +1,471 @@
+#Palavras com a letra a
+dicionario_letra_a_portugues = {
+    "Arara": "Abujene",
+    "Cigarro": "Ajamara",
+    "Tabaco": "Aja / Yaroto",
+    "Lixo": "Anana",
+    "Pela manhã": "Aniako",
+    "Palmito": "Anare",
+    "Hoje": "Ama",
+    "Mal": "Asida",
+    "Tipo de amido proveniente do buritizeiro": "Aru",
+    "Filho": "Auka",
+}
+
+#Palavras com a letra a 
+dicionario_letra_a_espanhol = {
+    "Guacamayo": "Abujene",
+    "Cigarrillo": "Ajamara",
+    "Tabaco": "Aja / Yaroto",
+    "Basura": "Anana",
+    "Por la mañana": "Aniako",
+    "Palmito": "Anare",
+    "Hoy": "Ama",
+    "Malo": "Asida",
+    "Yuruma": "Aru",
+    "Hijo": "Auka",
+}
+
+
+#Palavras com a letra b
+dicionario_letra_b_portugues = {
+    "Barco Grande": "Baroko",
+    "Cachorro": "Bejoro",
+    "Libélula": "Bijibiji",
+    "Tigela": "Boroma",
+    "Ferimento": "Bosi",
+    "Mangue": "Bujú",
+    "Dinheiro": "Burata",
+    "Tipo de Banana": "Buratana",
+    "Loco": "Burebaka",
+}
+
+#Palavras com a letra b
+dicionario_letra_b_espanhol = {
+    "Barco": "Baroko",
+    "Perro": "Bejoro",
+    "Caballito del diablo": "Bijibiji",
+    "Ponchera": "Boroma",
+    "Llaga": "Bosi",
+    "Mangle": "Bujú",
+    "Dinero": "Burata",
+    "Cambur": "Buratana",
+    "Loco": "Burebaka"
+}
+
+
+#Palavras com a letra d
+dicionario_letra_d_portugues = {
+    "Faca": "Dabo",
+    "Irmão pequeno": "Daka",
+    "Lança": "Daborabaka",
+    "Irmão": "Dakobo",
+    "Tio": "Daku",
+    "Tia": "Danikatida",
+    "Janela": "Dara",
+    "Tronco de madeira": "Daju",
+    "Conto": "Deje",
+    "Pai": "Dima",
+    "Inga": "Dojo",
+    "Pássaro": "Domu",
+    "Pájaro Rojo": "Domu simu",
+    "Cadeira": "Dujunoko",
+}
+
+#Paalvras com a letra d
+dicionario_letra_d_espanhol = {
+    "Cuchillo": "Dabo",
+    "Hermano pequeño": "Daka",
+    "Lanza": "Daborabaka",
+    "Hermano": "Dakobo",
+    "Tío": "Daku",
+    "Tía": "Danikatida",
+    "Ventana": "Dara",
+    "Tronco de madera": "Daju",
+    "Cuento": "Deje",
+    "Padre": "Dima",
+    "Guamo": "Dojo",
+    "Pájaro": "Domu",
+    "Pájaro Rojo": "Domu simu",
+    "Silla": "Dujunoko",
+}
+
+
+#Palavras com a letra e
+dicionario_letra_e_portugues = {
+    "Boto": "Ebute",
+    "Dentro": "Eku",
+    "Zero/Nada": "Ekida",
+    "Boia de buriti": "Enejero",
+    "Cipó": "Ero",
+}
+
+#Palavras com a letra e
+dicionario_letra_e_espanhol = {
+    "Tonina": "Ebute",
+    "Dentro": "Eku",
+    "Zero": "Ekida",
+    "Boya de buriti": "Enejero",
+    "Bejuco": "Ero",
+}
+
+
+#Palavras com a letra i
+dicionario_letra_i_portugues = {
+    "Graviola": "Ibakuja",
+    "Medicamento": "Ibiji",
+    "Garota": "Iboma",
+    "Adulto": "Idamo",
+    "Tu": "Iji",
+    "Professor": "Inaminatu",
+    "Eu": "Ine",
+    "Meia noite": "Imautu",
+    "Um": "Isaka",
+}
+
+#Palavras letra com a i
+dicionario_letra_i_espanhol = {
+    "Guanábana": "Ibakuja",
+    "Medicina": "Ibiji",
+    "Muchacha": "Iboma",
+    "Adulto": "Idamo",
+    "Tú": "Iji",
+    "Maestro": "Inaminatu",
+    "Yo": "Ine",
+    "Media noche": "Imautu",
+    "Uno": "Isaka",
+}
+
+
+#Palavras letra com a j
+dicionario_letra_j_portugues = {
+    "Capivara": "Jaba",
+    "Lápis": "Jabatakoino",
+    "Remo": "Jaje",
+    "Igarapé, Riacho": "Jana",
+    "Habitação Warao": "Janoko",
+    "Tucano": "Jari",
+    "Arco": "Jataburu",
+    "Caranguejo": "Je",
+    "Borracha": "Jeberekoino",
+    "Espírito": "Jebu",
+    "Fumaça": "Jejuku",
+    "Cozinha": "Jisabanoko",
+    "Machado": "Jima",
+    "Água": "Jo",
+    "Feiticeiro bom": "Joarotu",
+    "Terra": "Jobaji",
+    "Cachaça": "Jobi",
+}
+
+#Palavras com a letra j
+dicionario_letra_j_espanhol = {
+    "Chigüiri": "Jaba",
+    "Lápiz": "Jabatakoino",
+    "Canalete": "Jaje",
+    "Caño": "Jana",
+    "Vivienda Warao": "Janoko",
+    "Tucán": "Jari",
+    "Arco": "Jataburu",
+    "Cangrejo": "Je",
+    "Borrador": "Jeberekoino",
+    "Espíritu": "Jebu",
+    "Humo": "Jejuku",
+    "Cocina": "Jisabanoko",
+    "Hacha": "Jima",
+    "Agua": "Jo",
+    "Brujo bueno": "Joarotu",
+    "Tierra": "Jobaji",
+    "Aguardiente": "Jobi",
+}
+
+
+#Palavras letra k
+dicionario_letra_k_portugues = {
+    "Cinturão": "Kabekoa",
+    "Calça": "Kabesecuju",
+    "Caderno": "Karata",
+    "Escola": "Karata Waranoko",
+    "Bem-te-vi": "Keri",
+    "Todos": "Kokotuka",
+    "Pé": "Komu",
+    "Papagaio": "Koro",
+    "Cutia": "Kuamara",
+    "Saíram Correndo": "Kurujubuae",
+}
+
+#Palavras letra k
+dicionario_letra_k_espanhol = {
+    "Cinturón": "Kabekoa",
+    "Pantalón": "Kabesecuju",
+    "Cuaderno": "Karata",
+    "Escuela": "Karata Waranoko",
+    "Cristo fue": "Keri",
+    "Todos": "Kokotuka",
+    "Pie": "Komu",
+    "Loro": "Koro",
+    "Acure": "Kuamara",
+    "Salieron Corriendo": "Kurujubuae",
+}
+
+
+#Palavras com a letra m
+dicionario_letra_m_portugues = {
+    "Dois": "Manamo",
+    "Veado": "Masi",
+    "Iguana": "Mera",
+    "Cinco": "Mojabasi",
+    "Semejante": "Monika",
+    "Mão": "Mojo",
+    "Dez": "Mojoreko",
+    "Gato": "Misi",
+    "Sonho": "Musibu",
+    "Aliança de Casamento": "Musikuju",
+    "Osso": "Muju",
+}
+
+#Palavras  com a letra m
+dicionario_letra_m_espanhol = {
+    "Dos": "Manamo",
+    "Venado": "Masi",
+    "Iguana": "Mera",
+    "Cinco": "Mojabasi",
+    "Semejante": "Monika",
+    "Mano": "Mojo",
+    "Diez": "Mojoreko",
+    "Gato": "Misi",
+    "Sueño": "Musibu",
+    "Alianza de Casamiento": "Musikuju",
+    "Hueso": "Muju",
+}
+
+
+#Palavras com a letra n
+dicionario_letra_n_portugues = {
+    "Onda pequena": "Naba",
+    "Talher": "Nabaja",
+    "Espírito das águas": "Nabarão",
+    "Macaco": "Naku",
+    "Nuvem": "Najamutu",
+    "Raio": "Naja baribari",
+    "Chuva": "Naja",
+    "Comida": "Najoro",
+    "Restaurante": "Najoronoko",
+    "Abacaxi": "Nana",
+    "Avó": "Natu",
+    "Neto": "Natoro",
+    "Milho": "Naukamu",
+    "Homem": "Nebu",
+    "Masculino": "Nibora",
+    "Doença": "Nobara",
+    "Criança pequena": "Noboto",
+    "Fome": "Nojeira",
+}
+
+#Palavras com a letra n
+dicionario_letra_n_espanhol = {
+    "Ola pequeña": "Naba",
+    "Cubierto": "Nabaja",
+    "Madre de agua": "Nabarão",
+    "Mono": "Naku",
+    "Nube": "Najamutu",
+    "Rayo": "Naja baribari",
+    "Lluvia": "Naja",
+    "Comida": "Najoro",
+    "Restaurante": "Najoronoko",
+    "Piña": "Nana",
+    "Abuela": "Natu",
+    "Nieto": "Natoro",
+    "Maíz": "Naukamu",
+    "Hombre": "Nebu",
+    "Masculino": "Nibora",
+    "Enfermedad": "Nobara",
+    "Niño o niña pequeña": "Noboto",
+    "Hambre": "Nojeira",
+}
+
+
+#Palavras com a letra o
+dicionario_letra_o_portugues = {
+    "Buriti": "Oji",
+    "Mata de moriche": "Ojidú",
+    "Cachicamo (Tatu)": "Oka",
+    "Nós": "Oko",
+    "Sapato": "Omunamu",
+}
+
+#Palavras com a letra o
+dicionario_letra_o_espanhol = {
+    "Moriche": "Oji",
+    "Mata de moriche": "Ojidú",
+    "Cachicamo": "Oka",
+    "Nosotros": "Oko",
+    "Zapato": "Omunamu",
+}
+
+
+#Palavras com a letra s
+dicionario_letra_s_portugues = {
+    "Alergia": "Sabasa",
+    "Morcego": "Saja",
+    "Caranguejo pequeno": "Sakaro",
+    "Louco": "Sinakabaka",
+    "Cana de açúcar": "Sikaro",
+}
+
+#Palavras com a letra s
+dicionario_letra_s_espanhol = {
+    "Alérgico": "Sabasa",
+    "Murciélago": "Saja",
+    "Cangrejo pequeño": "Sakaro",
+    "Loco": "Sinakabaka",
+    "Caña de azúcar": "Sikaro",
+}
+
+
+#Palavras com a letra t
+dicionario_letra_t_portugues = {
+    "Eles": "Tatuma",
+    "Pião": "Tejere",
+    "Corpo": "Tejo",
+    "Pavão": "Teko",
+    "Mulher": "Tida",
+    "Tigre": "Tobe",
+    "Papagaio pequeno": "Toromu",
+}
+
+#Palavras com a letra t
+dicionario_letra_t_espanhol = {
+    "Ellos": "Tatuma",
+    "Trompo": "Tejere",
+    "Cuerpo": "Tejo",
+    "Pavo real": "Teko",
+    "Mujer": "Tida",
+    "Tigre": "Tobe",
+    "Loro": "Toromu",
+}
+
+#Palavras com a letra u
+dicionario_letra_u_portugues = {
+    "Dormindo": "Ubaya",
+    "Cesto": "Uju",
+    "Pato": "Ume",
+    "Fundo": "Unida",
+    "Cará": "Ure",
+    "Ciriguela": "Usi",
+}
+
+#Palavras com a letra u
+dicionario_letra_u_espanhol = {
+    "Durmiendo": "Ubaya",
+    "Mapire": "Uju",
+    "Pato": "Ume",
+    "Fondo": "Unida",
+    "Ocumo Chino": "Ure",
+    "Jobo": "Usi",
+}
+
+
+#Palavras com a letra w
+dicionario_letra_w_portugues = {
+    "Canoa": "Wajibaka",
+    "Lua": "Waniko",
+    "Pessoa Warao": "Warao",
+    "Borboleta": "Warowaro",
+    "Pomba": "Wiji",
+    "Feiticeiro": "Wisiratu",
+}
+
+#Palavaras com a letra w
+dicionario_letra_w_espanhol = {
+    "Curiera": "Wajibaka",
+    "Luna": "Waniko",
+    "Persona Warao": "Warao",
+    "Mariposa": "Warowaro",
+    "Paloma": "Wiji",
+    "Brujo": "Wisiratu",
+}
+
+
+#Palavras com a letra y
+dicionario_letra_y_portugues = {
+    "Pescadores": "Yabamo",
+    "Pescador": "Yabatu",
+    "Dia": "Yaja",
+    "Bom": "Yakera",
+    "Trabalho": "Yaota",
+    "Trabalhador": "Yaotaquitane",
+    "Eles": "Yatu",
+}
+
+#Palavras com a letra y
+dicionario_letra_y_espanhol = {
+    "Pescadores": "Yabamo",
+    "Pescador": "Yabatu",
+    "Día": "Yaja",
+    "Bueno": "Yakera",
+    "Trabajo": "Yaota",
+    "Trabajador": "Yaotaquitane",
+    "Ellos": "Yatu",
+}
+
+
+dicionario_saudacoes_portugues = {
+    "Bom dia": "Yakera Yokonae",
+    "Boa tarde": "Yakera Anakuarika",
+    "Boa noite": "Yakera Imaya",
+    "Como estás?": "Katukete",
+    "Tenho saúde, estou bem": "Bajukaya",
+    "Nós não vamos": "Diana oko naruya",
+    "Adeus": "Diana omi Nariya"
+}
+
+dicionario_saudacoes_espanhol = {
+    "Buenos días": "Yakera Yokonae",
+    "Buenas tardes": "Yakera Anakuarika",
+    "Buenas noches": "Yakera Imaya",
+    "¿Qué tal?": "Katukete",
+    "Tengo salud, estoy bien": "Bajukaya",
+    "Nosotros ya no vamos": "Diana oko naruya",
+    "Adiós": "Diana omi Nariya",
+}
+
+#Palavras sobre a parte do corpo
+dicionario_escolar_portugues = {
+    "Escola": "Karata Waranoko",
+    "Caderno": "Karata",
+    "Lápis": "Jabatakoino",
+    "Borracha": "Jabata",
+    "Apontador": "Jabatakoino ai nisakoino",
+    "Folha de papel A4": "Karata ajoro joko",
+    "Regra": "Asinaria nisakoino",
+    "Lápis de cor": "Jabatakoino ajaba isiko",
+    "Livro de Leitura": "Karata deje awarakoino",
+    "Cadeira": "Dujunoko",
+    "Mesa": "Daju basa",
+    "Professor": "Inaminatu",
+    "Estudantes": "Karata awamaro",
+    "Biblioteca": "Karata deje awarakoino abanko",
+    "Refeitório": "Inanoronoko",
+    "Coordenador": "Inamimo aidamo",
+}
+
+#Palavras sobre a parte do corpo
+dicionario_escolar_espanhol = {
+    "Escuela": "Karata Waranoko",
+    "Cuaderno": "Karata",
+    "Lápiz": "Jabatakoino",
+    "Borrador": "Jabata",
+    "Sacapuntas": "Jabatakoino ai nisakoino",
+    "Hoja de Papel A4": "Karata ajoro joko",
+    "Reglas": "Asinaria nisakoino",
+    "Lápiz de colores": "Jabatakoino ajaba isiko",
+    "Libro de lectura": "Karata deje awarakoino",
+    "Silla": "Dujunoko",
+    "Mesa": "Daju basa",
+    "Profesor": "Inaminatu",
+    "Estudiantes": "Karata awamaro",
+    "Biblioteca": "Karata deje awarakoino abanko",
+    "Comedor": "Inanoronoko",
+    "Coordinador": "Inamimo aidamo",
+}
---
+++ data/glossario_3/dados.py
@@ -0,0 +1,281 @@
+#ESTE ARQUIVO SO CONTEM AS TRADUCOES DE PORTUGUES PARA WARAO, NAO CONTEM NADA EM ESPANHOL
+
+dicionario_letra_a = {
+    "Pescoço": "Aaro",
+    "Filho": "Auka",
+    "Filha": "Aukatida",
+    "Farinha": "Arukobo"
+}
+
+dicionario_letra_b = {
+    "Sal": "Bamu",
+    "Cunhado": "Barayatu",
+    "Cunhada": "Bigitida",
+    "Refrigerante": "Bereko",
+    "Barriga": "Bono",
+    "Banana": "Buratana"
+}
+
+dicionario_letra_b = {
+    "Mãe": "Daní",
+    "Pai": "Dima",
+    "Costas": "Dikaba",
+    "Vômito": "Dokojiya"
+}
+
+
+dicionario_letra_j = {
+    "Braço": "Jara",
+    "Anemia": "Jene",
+    "Nariz": "Jikari",
+    "Língua": "Jono",
+    "Coração": "Jokaro",
+    "Sede": "Jobajera",
+    "Inflamação": "Jokojoko",
+}
+
+
+dicionario_letra_k = {
+    "Rins": "Kamai",
+    "Fígado": "Kaya",
+    "Joelho": "Kamukuru",
+    "Orelha": "Kojoko",
+    "Cérebro": "Kuabo"
+}
+
+
+dicionario_letra_m = {
+    "Peito": "Mejo",
+    "Olho": "Mu",
+    "Unha": "Mojosí"
+}
+
+
+dicionario_letra_n = {
+    "Avó": "Natu",
+    "Vovô": "Nobo",
+    "Fome": "Nojera",
+    "Bebê": "Noboto",
+    "Tomar banho": "Nakokitane"
+}
+
+
+dicionario_letra_o = {
+    "Quatro": "Orabakaya",
+    "Gripe": "Obo",
+    "Pé": "Omu"
+}
+
+
+dicionario_letra_y = {
+    "Segunda-feira": "Yaotaisaka",
+    "Bom dia": "Yakera jokonera",
+    "Boa tarde": "Yakera anakuarika",
+    "Boa noite": "Yakera imaya"
+}
+
+
+dicionario_frases_interacao = {
+    "Bom dia": "Yakera jokonera",
+    "Boa tarde": "Yakera anakuarika",
+    "Sou indígena Warao": "Ine warao",
+    "Sou migrante": "Ine jobai daisayatamo",
+    "Sou refugiado": "Ine jobai daisaeku ubanko",
+    "Preciso ser atendido": "Kasaba warakitane",
+    "Preciso de ajuda": "Masanetakitane",
+    "Estou doente": "Ine nobaraya",
+    "Ele está doente": "Iji nobaraya",
+    "Ela está doente": "Tai nobaraya",
+    "Como você se chama?": "Ji wai sina? / Ji wai katukane?",
+    "Estou aguardando para ser atendido": "Mate ine wakaya masaba warakitane",
+    "Este é o meu documento": "Tamaja tai makarata",
+    "Meu endereço é...": "Majanoko a wai",
+}
+
+
+dicionario_frases_localizacao = {
+    "Onde fica a sala?": "Kasaba bajiya yarokotanoko",
+    "Onde está a enfermeira?": "Kasaba ja yarokotatu",
+    "Onde está o médico?": "Kasaba ja ibajukatu",
+    "Onde é a sala de exames?": "Jotu misanoko kasaba bajiya?",
+    "Onde é a sala de medicamentos?": "Kasaba bajiya yarokota nowanoko?",
+    "Onde fica o banheiro?": "Kasaba bajiya jibonoko?"
+}
+
+
+dicionario_frases_informativas = {
+    "Tenho alergia a essa medicação": "Ine yarokota tai isia tijtijida",
+    "Estou fazendo tratamento Warao": "Ine ja warao ayarokota isia",
+    "Qual medicamento vou tomar?": "Yarokota sina mamowate?"
+}
+
+
+dicionario_partes_corpo = {
+    "Orelha": "Kojoko",
+    "Olho": "Mu",
+    "Testa": "Tukaba",
+    "Nariz": "Jikari",
+    "Boca": "Doko",
+    "Língua": "Jono",
+    "Pescoço": "Aro",
+    "Seio": "Kami",
+    "Braço": "Jara",
+    "Cotovelo": "Bojoka",
+    "Mão": "Mojo",
+    "Unha": "Mojosí",
+    "Vagina": "Jokai",
+    "Pênis": "Wata",
+    "Mindinho": "Mênique",
+    "Cabelo" : "Jio",
+    "Cérebro": "Kuabo",
+    "Cabeça": "Kuá",
+    "Costas": "Dikaba",
+    "Peito": "Mejo",
+    "Bunda": "Jomuju",
+    "Barriga": "Bono",
+    "Nádegas": "Abate",
+    "Perna": "Kaja",
+    "Joelho": "Kamukuru",
+    "Anteperna": "Noi",
+    "Canela": "Baji",
+    "Pé": "Omu",
+    "Garganta": "Waroba",
+    "Costela": "Bamuju",
+    "Rins": "Kamai",
+    "Vesícula": "Bokabe",
+    "Fígado": "Kaya",
+    "Intestino": "Asomosomoko",
+    "Coração": "Jokaro",
+    "Estômago": "Obono Najoro Ajanoko",
+}
+
+
+dicionario_saude = {
+    "Gripe": "Obo",
+    "Coriza": "Obo ainabara",
+    "Nariz entupido": "Nariz tapada / Obo aikari mokoronae",
+    "Tosse": "Waroba tiji tiji",
+    "Falta de ar": "Jajina yakera nabuaranaja",
+    "Dor de barriga / Dor de estômago": "Makobe ajera",
+    "Diarreia": "Jovito kimiya",
+    "Vômito": "Dokojiya",
+    "Constipação": "Joto tai",
+    "Estou sentindo dor": "Masia ajera",
+    "Vermes": "Kobejuba",
+    "Coceira": "Tiji tiji",
+    "Corte ou ferida": "Kabatanae",
+    "Inflamação": "Jokojoko",
+    "Sede": "Jobajera",
+    "Não consigo comer": "Nojo ekida",
+    "Fome": "Nojera",
+    "Não consigo dormir": "Ine obakumoni",
+    "Infecção": "Daisama majaya",
+    "Estou menstruando": "Ine mimai",
+    "Já menstruei": "Ine mimaiya",
+    "Absorventes": "Joto anamo",
+    "Não está chegando a menstruação": "Ine nemanja ine nejomanaja",
+    "Tenho cólica / dor": "Mobokabe ajera",
+    "Suspeito que estou grávida": "Ine kuaboyatai iana",
+    "Estou grávida": "Ine kuabu",
+    "Sou casada": "Ori ni saani",
+    "Não tenho filhos": "Manoboto ekida",
+    "Você faz alguma coisa para não engravidar?": "Yarokota kuruya kuabuanka katimate",
+    "Tive vários parceiros sexuais": "Nibora daisama isiko ori monikatai",
+    "Tenho apenas um parceiro sexual": "Nibo eraja asiiko ori monikatai",
+    "Não tive relações sexuais": "Totona",
+    "Não tenho relações sexuais": "No tengo relaciones sexuales",
+    "Mate tida ainaja (falei mulher)": "Se quem fala é mulher",
+    "Mate tida ajiina (falei homem)": "Se quem fala é homem",
+    "Vacinas": "Akosa abakitaine",
+    "Aleitar": "Ami orokitane",
+    "Hipertensão": "Botobetera",
+    "Hérnia": "Kabukari nabutónai",
+    "Pus": "Kunaje / Onobo",
+    "Tuberculose": "Obosabana",
+    "Enxaqueca": "Kawajera abane akuajera",
+    "Doença sexualmente transmissível": "Samorona",
+    "Infarto": "Jokarosinaka",
+    "Sangue": "Joto",
+    "Anemia": "Jene",
+    "HIV - AIDS": "Wabasabana",
+    "Diabetes": "Jotu diabeira",
+    "Câncer": "Ekusbana",
+}
+
+
+dicionario_higiene = {
+    "Lavar os alimentos": "Najoro jaraiya / Najoro joaria",
+    "Tomar banho": "Nakokitane",
+    "Piolho": "Ami",
+    "Lixo": "Ajão - Ajab",
+    "Verme": "Joba ji ajuba",
+    "Escovar os dentes": "Ai jaranta nai"
+}
+
+
+dicionario_alimentos = {
+    "Frango": "Karina atoma",
+    "Refrigerante": "Bereco",
+    "Sopa": "Isaba",
+    "Sal": "Bamu",
+    "Peixe": "Jonakaba",
+    "Açúcar": "Daberaja",
+    "Laranja": "Naranja",
+    "Banana": "Buratana",
+    "Farinha": "Arukobo",
+    "Carne": "Ina atoma",
+}
+
+
+dicionario_parentesco = {
+    "Marido": "Anibora",
+    "Avó": "Natu",
+    "Esposa": "Atida",
+    "Tia": "Danikatida",
+    "Tio": "Daku",
+    "Mãe": "Daní",
+    "Pai": "Dima",
+    "Irmão mais velho": "Daje",
+    "Filho": "Auka",
+    "Filha": "Aukatida",
+    "Irmão mais novo": "Daka",
+    "Bebê": "Noboto",
+    "Namorado": "Aturbita",
+    "Cunhado": "Bayaratu",
+    "Neto": "Natoro",
+}
+
+
+dicionario_numeros = {
+    "Um": "Isaka",
+    "Dois": "Manamo",
+    "Três": "Dianamo",
+    "Quatro": "Orabakaya",
+    "Cinco": "Muabasi",
+    "Seis": "Mojo amata isaka",
+    "Sete": "Mojo amata manamo",
+    "Oito": "Mojo amata dianamo",
+    "Nove": "Mojo amata orabakaja",
+    "Dez": "Mojoreko",
+    "Onze": "Mojoreko arai isaka",
+    "Doze": "Mojoreko arai manamo",
+    "Treze": "Mojoreko arai dianamo",
+    "Catorze": "Mojoreko arai orabakaya",
+    "Quinze": "Mojoreko arai muabasi",
+    "Dezesseis": "Mojoreko arai muabasi mojo amata isaka",
+    "Dezessete": "Mojoreko arai muabasi mojo amata manama",
+    "Dezoito": "Mojoreko arai muabasi mojo amata dianamo",
+    "Dezenove": "Mojoreko arai muabasi mojo amata orabakaja",
+    "Vinte": "Warao isaka",
+}
+
+
+dicionario_dias_semana = {
+    "Segunda-feira": "Yaotaisaka",
+    "Terça-feira": "Yaotaaraisa",
+    "Quarta-feira": "Yaotariana",
+    "Quinta-feira": "Yaotoarawabaakia",
+    "Sexta-feira": "Yaotamoabasi",
+    "Sábado": "Misaraka",
+    "Domingo": "Misa",
+}
