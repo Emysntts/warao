a362025dc93c9dbd208a494f938be78dd073ed17
Carlos Santos <95683199+carlimmsantos@users.noreply.github.com>
2025-05-01 20:40:28 -0300

    styles["QuestionStyles"] - Ajustando estilos do container da pergunta para incluir altura e centralização

--- warao-front/components/Question/styles.ts
+++ warao-front/components/Question/styles.ts
@@ -6,12 +6,14 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 12,
     alignItems: 'center',
+    justifyContent: 'center',
     marginBottom: 16,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
-    elevation: 5, // Sombra para Android
+    elevation: 5, 
+    height: "35%",
   },
   questionText: {
     fontSize: 16,
