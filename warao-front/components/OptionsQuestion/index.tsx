8c329805ab03783c6b1992fdb3f9a587f2f3a054
Carlos Santos <95683199+carlimmsantos@users.noreply.github.com>
2025-05-01 20:22:33 -0300

    feat["OptionsQuestion"] - Removendo componente OptionsQuestion e adicionando o correto

--- warao-front/components/OptionsQuestion/index.tsx
+++ warao-front/components/OptionsQuestion/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import styles from './styles';
 
-const Options = ({ options }) => {
+const Options = ({ options }: { options: string[] }) => {
   return (
     <View style={styles.optionsContainer}>
       {options.map((option, index) => (
