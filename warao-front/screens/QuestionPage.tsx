13c5d454af7e85ae6bb2749633694737d0c42db6
Carlos Santos <95683199+carlimmsantos@users.noreply.github.com>
2025-05-01 21:51:24 -0300

    styles[QuestionPage] - Adicionando StatusBar para melhor visibilidade e estilo

--- warao-front/screens/QuestionPage.tsx
+++ warao-front/screens/QuestionPage.tsx
@@ -3,11 +3,14 @@ import { View, StyleSheet } from 'react-native';
 import Header from '../components/HeaderQuestion';
 import Question from '../components/Question';
 import Options from '../components/OptionsQuestion';
+import { StatusBar } from 'expo-status-bar';
 
 const QuestionPage = () => {
   return (
-    <View style={styles.container}>
 
+    
+    <View style={styles.container}>
+      <StatusBar style="light" backgroundColor="#6A5ACD" />
       <Header title="Saudações" />
 
       <View style={{ padding: 16 }}>
