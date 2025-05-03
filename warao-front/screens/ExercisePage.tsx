cc4fd6dd757000557957c23bba43d874b0561dd5
Carlos Santos <95683199+carlimmsantos@users.noreply.github.com>
2025-05-03 12:01:57 -0300

    fix(ExercisePage) - Criando uma lista aonde nela vai ter os modulos dos projetos e passando as informções para o ExerciseComp

--- warao-front/screens/ExercisePage.tsx
+++ warao-front/screens/ExercisePage.tsx
@@ -3,11 +3,62 @@ import { Platform } from 'expo-modules-core';
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView } from 'react-native-gesture-handler';
 export const ExercisePage = () => {
+
+  const modulos= [
+    {
+      id: 1,
+      title: 'Saudações',
+      isUnlocked: true
+    },
+    {
+      id: 2,
+      title: 'Apresentações',
+      isUnlocked: true
+    },
+    {
+      id: 3,
+      title: 'Números',
+      isUnlocked: false
+    },
+    {
+      id: 4,
+      title: 'Cores',
+      isUnlocked: false
+    },
+    {
+      id: 5,
+      title: 'Animais',
+      isUnlocked: false
+    },
+    {
+      id: 6,
+      title: 'Comida',
+      isUnlocked: false
+    },
+    {
+      id: 7,
+      title: 'Roupas',
+      isUnlocked: false
+    },
+    {
+      id: 8,
+      title: 'Verbos',
+      isUnlocked: false
+    },
+  ]
+
+
+
+
   return (
     <>
       <ScrollView className="bg-warao-sandcolor flex h-full flex-col p-5">
-        {Array.from({ length: 10 }).map((_, i) => (
-          <ExerciseComp key={i} text="Saudações" isUnlocked />
+        {modulos.map((modulo) => (
+          <ExerciseComp 
+            key={modulo.id} 
+            text={modulo.title} 
+            isUnlocked={modulo.isUnlocked} 
+          />
         ))}
       </ScrollView>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
