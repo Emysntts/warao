ade1e8df716dd5d9e37d92c277b22428dee2681b
Carlos Santos <95683199+carlimmsantos@users.noreply.github.com>
2025-05-03 12:02:14 -0300

    fix(HeadModules) - Passando o título como prop para o componente HeadModules

--- warao-front/components/HeadModules/index.tsx
+++ warao-front/components/HeadModules/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
 
-const HeadModules = () => {
+const HeadModules = ({titulo}: {titulo: string}) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Modulos</Text>
+      <Text style={styles.text}>Modulo - {titulo}</Text>
     </View>
   );
 };
--- warao-front/screens/ModulesPage.tsx
+++ warao-front/screens/ModulesPage.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, SafeAreaView } from 'react-native';
-import CustomTabNavigator from 'components/CustomTabNavigator';
 import HeadModules from 'components/HeadModules';
 import ExerciseLauncher from 'components/ExerciseLauncher';
 import { ScrollView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import { useRoute, RouteProp } from '@react-navigation/native';
+import { StackExerciseLists } from 'navigation/stack-exercise-navigation';
+
 
 const ModulesPage = () => {
   const colorPalette = [
@@ -16,12 +18,14 @@ const ModulesPage = () => {
     '#8A2BE2', // Azul
     '#FF4500', // Vermelho
   ];
+  const route = useRoute<RouteProp<StackExerciseLists, 'modules'>>();
+  const { text } = route.params; // Recebe o texto enviado
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor='#3B2414' style="light" />
 
-      <HeadModules />
+      <HeadModules titulo={text}  />
 
       <ScrollView contentContainerStyle={styles.scrollContent}>
         {colorPalette.map((color, index) => (
