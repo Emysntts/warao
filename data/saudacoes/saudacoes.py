da180de308af11006f8042f889d4e61b6d525929
Vitoreism <vitoreism@gmail.com>
2025-05-03 18:20:57 -0300

    Funcoes Gerar pergutas

---
+++ data/saudacoes/saudacoes.py
@@ -0,0 +1,19 @@
+dicionario_saudacoes_portugues = {
+    "Bom dia": "Yakera Yokonae",
+    "Boa tarde": "Yakera Anakuarika",
+    "Boa noite": "Yakera Imaya",
+    "Como estás?": "Katukete",
+    "Tenho saúde, estou bem": "Bajukaya",
+    "Nós não vamos": "Diana oko naruya",
+    "Adeus": "Diana omi Nariya"
+}
+
+dicionario_saudacoes_espanhol = {
+    "Buenos días": "Yakera Yokonae",
+    "Buenas tardes": "Yakera Anakuarika",
+    "Buenas noches": "Yakera Imaya",
+    "¿Qué tal?": "Katukete",
+    "Tengo salud, estoy bien": "Bajukaya",
+    "Nosotros ya no vamos": "Diana oko naruya",
+    "Adiós": "Diana omi Nariya",
+}
