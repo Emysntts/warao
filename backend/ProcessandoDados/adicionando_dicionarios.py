e06bb217c6fd4ba7e31d43d7b04f62ed8dfe9f6a
Carlos Santos <95683199+carlimmsantos@users.noreply.github.com>
2025-05-04 11:10:36 -0300

    Feat [adicionando_dicionarios'] - colocando uma logica para que ele não salve os dicionario sem necessidade

--- backend/ProcessandoDados/adicionando_dicionarios.py
+++ backend/ProcessandoDados/adicionando_dicionarios.py
@@ -18,3 +18,14 @@ def add_word_to_db(word_data):
     except requests.exceptions.RequestException as e:
         print(f"Error adding word to DB: {e}")
         return None
+
+
+def is_database_empty() -> bool:
+    try:
+        response = requests.get("http://127.0.0.1:8000/palavras/")
+        response.raise_for_status()
+        data = response.json()
+        return len(data) == 0  # Retorna True se o banco estiver vazio
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao verificar o banco de dados: {e}")
+        return True  # Assume que está vazio em caso de erro
