833bb7d9f7ee5fccddacbc11bde55c060d16394b
JonatasMSS <jonatas.miguelss@gmail.com>
2025-05-04 00:35:14 -0300

    feat(TabNavigator) - Add About Us page to tab navigator

--- warao-front/navigation/tab-navigator.tsx
+++ warao-front/navigation/tab-navigator.tsx
@@ -1,6 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Header from 'components/Header';
+import AboutUsPage from 'screens/AboutUs';
 
 import { StackRouteExerciseLayout } from './stack-exercise-navigation';
 
@@ -10,20 +11,32 @@ export default function TabLayout() {
   return (
     <Tab.Navigator
       screenOptions={{
-      
         animation: 'shift',
         tabBarShowLabel: false,
         tabBarActiveBackgroundColor: '#3B2414',
-        
-        tabBarIcon: () => <Ionicons name="home" size={28} color="white" />,
+        tabBarInactiveBackgroundColor: '#523622',
         tabBarStyle: {
           borderTopLeftRadius: 10,
           borderTopRightRadius: 10,
+          height: 55,
         },
       }}>
-      <Tab.Screen component={StackRouteExerciseLayout} name="Tarefas" options={{
-        headerShown:false
-      }} />
+      <Tab.Screen
+        component={StackRouteExerciseLayout}
+        name="Tarefas"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => <Ionicons name="home" size={28} color="white" />,
+        }}
+      />
+      <Tab.Screen
+        component={AboutUsPage}
+        name="aboutus"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => <Ionicons name="help-circle" size={30} color="white" />,
+        }}
+      />
     </Tab.Navigator>
   );
 }
