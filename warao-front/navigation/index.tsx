222fcc8e1c02ddba7287ca311f2cf6b161377d78
JonatasMSS <jonatas.miguelss@gmail.com>
2025-05-05 17:58:21 -0300

    fix: update navigation structure to include PresentationPage and adjust parameter types

--- warao-front/navigation/index.tsx
+++ warao-front/navigation/index.tsx
@@ -1,16 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import PresentationPage from 'screens/Presentation';
+import QuestionPage from 'screens/QuestionPage';
 
 import TabNavigator from './tab-navigator';
 import HomeScreen from '../screens/HomeScreen';
 import ModulesPage from '../screens/ModulesPage';
-import QuestionPage from 'screens/QuestionPage';
 
 export type RootStackParamList = {
   Home: undefined;
   TabNavigator: undefined;
   ModulesPage: undefined;
-  QuestionPage: { moduleText: string, quantidadePerguntas: number };
+  PresentationPage: undefined;
+  QuestionPage: { moduleText: string; quantidadePerguntas: number };
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -19,11 +21,16 @@ export default function RootStack() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
+          name="PresentationPage"
+          component={PresentationPage}
+          options={{
+            headerShown: false,
+          }}
         />
+
         <Stack.Screen
           name="TabNavigator"
           component={TabNavigator}
@@ -35,10 +42,7 @@ export default function RootStack() {
           component={QuestionPage}
           options={{ headerShown: false }}
         />
-
-   
       </Stack.Navigator>
-      
     </NavigationContainer>
   );
 }
